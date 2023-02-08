eb9c01f482ebeddb822b620cf03e5529d9c7aa6f
qwersfnk <gaomuyaye06@gmail.com>
2023-02-08 12:40:55 +0000

    テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource_or_scope)
+    if resource.is_a?(Admin)
+      admin_camp_facilities_path
+    else
+      root_path
+    end
+  end
 end
