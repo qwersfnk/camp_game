f1109989728d9f61e4e22161e9af4edbd5dfcc71
qwersfnk <gaomuyaye06@gmail.com>
2023-02-15 11:37:19 +0000

    appの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     if resource.is_a?(Admin)
       admin_camp_facilities_path
+      return
     else
       user_path(resource)
     end
